0ef4bcf2819c15ee92c2f1f0f108946878ec7124
thuang3316 <249622692+thuang3316@users.noreply.github.com>
2025-12-28 21:35:45 -0500

    add button functions for home, menu and about

--- src/index.js
+++ src/index.js
@@ -3,6 +3,37 @@ import createHomePage from "./home.js";
 import createMenu from "./menu.js";
 import createAbout from "./about.js";
 
-createHomePage();
-createMenu();
-createAbout();
+const createPage = function () {
+    createHomePage();
+    createMenu();
+    createAbout();
+
+    const homeBtn = document.getElementById('home-btn');
+    const menuBtn = document.getElementById('menu-btn');
+    const aboutBtn = document.getElementById('about-btn');
+
+    const homePage = document.querySelector('.homePage');
+    const menuPage = document.getElementById('menu-grid');
+    const aboutPage = document.querySelector('.about');
+
+    homeBtn.addEventListener('click', () => {
+        homePage.style.display = 'flex';
+        menuPage.style.display = 'none';
+        aboutPage.style.display = 'none';
+    })
+
+    menuBtn.addEventListener('click', () => {
+        homePage.style.display = 'none';
+        menuPage.style.display = 'grid';
+        aboutPage.style.display = 'none';
+    })
+
+    aboutBtn.addEventListener('click', () => {
+        homePage.style.display = 'none';
+        menuPage.style.display = 'none';
+        aboutPage.style.display = 'block';
+    })
+};
+
+createPage();
+
